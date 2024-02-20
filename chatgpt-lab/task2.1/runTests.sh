33b5777cb629c1ededb6a329be0bc6d2eb706841
RedPenguin88 <105743189+RedPenguin88@users.noreply.github.com>
2024-02-20 19:58:56 +0000

    add initial script with duplilcation issues

---
+++ chatgpt-lab/task2.1/runTests.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+# Put this script in your PhaseX directory
+
+usage() {
+	echo "Usage:    $0 <regular|unit>"
+	echo "Options: "
+	echo "          regular   use regular tests from /home/matt/CSC460/Espresso/Espresso-Handout-All-Phases.tar @ java.cs.unlv.edu"
+	echo "          unit      use unit tests from Canvas (requires manual download)"
+
+	exit "${1:-0}"
+}
+
+clean() {
+	rm --force ./ourTestResults ./refTestResults ./test_changes.diff ./ourUnitTestResults ./refUnitTestResults ./unit_test_changes.diff
+}
+
+installRegularTests() {
+	echo "==================="
+	echo "Grabbing Test Files"
+	echo "==================="
+	../espressou install tests ../
+}
+
+# Options: $1 = script to run (espressoc/espressocr)
+#          $2 = file to write to
+#          $3 = grammar to test (Espresso, Espresso+, or Espresso*)
+generateResults() {
+	script_to_run="$1"
+	file_to_write_to="$2"
+	grammar="$3"
+
+	echo "================================="
+	echo "Running Good Tests for $grammar"
+	echo "================================="
+
+	echo "$grammar Good Tests" >> "$file_to_write_to"
+	echo "===================" >> "$file_to_write_to"
+
+	for file in "$testsdir/$grammar"/GoodTests/*; do
+		[ "${file##*.}" = "java" ] && $script_to_run "$file" >> "$file_to_write_to"
+	done
+
+	echo "================================="
+	echo "Running Bad Tests for $grammar"
+	echo "================================="
+
+	echo "$grammar Bad Tests" >> "$file_to_write_to"
+	echo "===================" >> "$file_to_write_to"
+
+	for file in "$testsdir/$grammar"/BadTests/*; do
+		[ "${file##*.}" = "java" ] && $script_to_run "$file" >> "$file_to_write_to"
+	done
+}
+
+main() {
+	[ -z "$1" ] && usage 1
+
+	phase_number="$(pwd | tail -c 2)"
+	if ! echo "$phase_number" | grep --quiet "[1-5]"; then
+		 echo "Error: Couldn't detect phase number. Make sure this script is in PhaseX (where X is the current phase)" && exit 1
+	fi
+	
+	case "$1" in
+		regular)
+			output=./ourTestResults
+			refoutput=./refTestResults
+			testsdir="../Tests/Tests/Phase$phase_number"
+			grammars="Espresso Espresso+ Espresso++"  # Edit this line to only process for one particular grammar/extension
+			finaldiff=./test_changes.diff
+			;;
+		unit)
+			output=./ourUnitTestResults
+			refoutput=./refUnitTestResults
+			testsdir="../unit_tests/$phase_number/"
+			grammars="Espresso Espresso_Plus Espresso_Star"
+			finaldiff=./unit_test_changes.diff
+			;;
+		clean) clean && exit 0 ;;
+		-h|--help)  usage ;;
+		*)          echo "Error: invalid parameter." >&2 && exit 1 ;;
+	esac
+
+	if [ ! -d "${testsdir%/*/*}" ]; then
+		if [ "$1" = regular ]; then
+			installRegularTests
+		else
+			echo "Copy unit_tests from Canvas into the parent directory (${PWD%/*}) before use!" && exit 1
+		fi
+	fi
+
+	[ ! -d ./bin ] && echo "bin directory not found! Compiling using ant..." && ant > /dev/null
+
+	rm -f "$output"
+
+	if [ ! -f "$refoutput" ]; then
+		echo "================================="
+		echo "Reference Test Results not found!"
+		echo "Running reference compiler once to generate reference results"
+		echo "================================="
+
+		for grammar in $grammars; do
+			generateResults ./espressocr "$refoutput" "$grammar"
+		done
+	fi
+
+	echo "================================="
+	echo "Running our compiler to generate our results"
+	echo "================================="
+
+	for grammar in $grammars; do
+		generateResults ./espressoc "$output" "$grammar"
+	done
+
+	# Trim file path names and line numbers so that both outputs match
+	sed --in-place --regexp-extended "s#((\.\.)?/(\w*\+?/)*)?(\w*\.java)[0-9.:\-]*#\4:#" "$output"
+	sed --in-place --regexp-extended "s#((\.\.)?/(\w*\+?/)*)?(\w*\.java)[0-9.:\-]*#\4:#" "$refoutput"
+	diff "$output" "$refoutput" > "$finaldiff"
+
+	echo "Done! Check $finaldiff to see any discrepancies between the output of our compiler versus the reference compiler"
+	echo "If there's nothing in the file. You are Good!"
+
+	exit 0
+}
+
+main "$@"
